5413fe233b6d9c7179f1d37cfc3413f74e0c9b27
Rushika Datta <gdrushikadatta@gmail.com>
2026-09-14 23:50:10 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (18.24%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> pairs={
+            {')','('},
+            {']','['},
+            {'}','{'}
+        };
+        for(char c:s){
+            if(c=='('||c=='['||c=='{'){
+                st.push(c);
+            } else {
+                if(st.empty()||st.top()!=pairs[c]){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
